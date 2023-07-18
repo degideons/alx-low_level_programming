21d5b4476203d04de2020ff1b43ea3fe7065824a
degideons <degideons49@gmail.com>
2023-07-18 05:40:20 -0700

    description added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
 void print_to_98(int n)
 {
 	int i;
