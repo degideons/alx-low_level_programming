a393ce6d9ec05b81f3b6ad154deb601b4545499a
degideons <degideons49@gmail.com>
2023-07-27 01:19:03 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,74 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * infinite_add - add 2 strings.
- * @n1: string1.
- * @n2: string2.
- * @r: buffer
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
  * @size_r: buffer size
- * Return: String with all letters in ROT13 base.
+ *
+ * Return: address of r or 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i, j, k, len1, len2, carry, sum;
-
-    // Calculate lengths of the input strings
-    for (len1 = 0; n1[len1]; len1++)
-        ;
-    for (len2 = 0; n2[len2]; len2++)
-        ;
-
-    // Check if the result can be stored in the buffer
-    if (len1 >= size_r || len2 >= size_r)
-        return 0;
-
-    // Initialize variables for addition
-    i = len1 - 1;
-    j = len2 - 1;
-    k = 0;
-    carry = 0;
-
-    // Perform addition from right to left
-    while (i >= 0 || j >= 0 || carry > 0)
-    {
-        // Get the digits to add (or zero if the numbers have different lengths)
-        int digit1 = (i >= 0) ? n1[i] - '0' : 0;
-        int digit2 = (j >= 0) ? n2[j] - '0' : 0;
-
-        // Calculate the sum of digits and carry
-        sum = digit1 + digit2 + carry;
-
-        // Store the result digit in the result buffer
-        if (k < size_r - 1) // Reserve space for the null terminator
-        {
-            r[k] = sum % 10 + '0';
-            k++;
-        }
-        else
-        {
-            return 0; // Result cannot be stored in the buffer, return 0
-        }
-
-        // Update carry for the next iteration
-        carry = sum / 10;
-
-        // Move to the next digits of the input numbers
-        i--;
-        j--;
-    }
-
-    // Add null terminator to the result string
-    r[k] = '\0';
-
-    // Reverse the result string to get the correct order
-    for (i = 0, j = k - 1; i < j; i++, j--)
-    {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-    }
-
-    return r;
+int i, j, k, l, m, n;
+for (i = 0; n1[i]; i++)
+;
+for (j = 0; n2[j]; j++)
+;
+if (i > size_r || j > size_r)
+return (0);
+m = 0;
+for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+{
+n = m;
+if (i >= 0)
+n += n1[i] - '0';
+if (j >= 0)
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
+m = n / 10;
+r[k] = n % 10 + '0';
+}
+r[k] = '\0';
+if (i >= 0 || j >= 0 || m)
+return (0);
+for (k -= 1, l = 0; l < k; k--, l++)
+{
+m = r[k];
+r[k] = r[l];
+r[l] = m;
+}
+return (r);
 }
