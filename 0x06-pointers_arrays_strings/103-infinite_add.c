763c9d11e14b4bf49ff39cf1c2dc04eddc81154b
degideons <degideons49@gmail.com>
2023-07-27 01:07:37 -0700

    that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,52 +8,67 @@
  * @size_r: buffer size
  * Return: String with all letters in ROT13 base.
  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
-	
-	while (n1[a_len] != '\0')
-		a_len++;
-	while (n2[b_len] != '\0')
-		b_len++;
-	if (a_len > b_len)
-		biggest = a_len;
-	else
-		biggest = b_len;
-	if ((biggest + 1) >= size_r)
-		return (0);
-	r[biggest + 1] = '\0';
-	
-	while (biggest >= 0)
-	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
-		if (a_len > 0 && b_len > 0)
-			sum = a + b + carry;
-		else if (a_len < 0 && b_len > 0)
-			sum = b + carry;
-		else if (a_len > 0 && b_len < 0)
-			sum = a + carry;
-		else
-			sum = carry;
-		
-		if (sum > 9)
-		{
-			carry = sum / 10;
-			sum = (sum % 10) + '0';
-		}
-		else
-		{
-			carry = 0;
-			sum = sum + '0';
-		}
-		r[biggest] = sum;
-		a_len--;
-		b_len--;
-		biggest--;
-	}
-	if (*(r) != 0)
-		return (r);
-	else
-		return (r + 1);
+    int i, j, k, len1, len2, carry, sum;
+
+    // Calculate lengths of the input strings
+    for (len1 = 0; n1[len1]; len1++)
+        ;
+    for (len2 = 0; n2[len2]; len2++)
+        ;
+
+    // Check if the result can be stored in the buffer
+    if (len1 >= size_r || len2 >= size_r)
+        return 0;
+
+    // Initialize variables for addition
+    i = len1 - 1;
+    j = len2 - 1;
+    k = 0;
+    carry = 0;
+
+    // Perform addition from right to left
+    while (i >= 0 || j >= 0 || carry > 0)
+    {
+        // Get the digits to add (or zero if the numbers have different lengths)
+        int digit1 = (i >= 0) ? n1[i] - '0' : 0;
+        int digit2 = (j >= 0) ? n2[j] - '0' : 0;
+
+        // Calculate the sum of digits and carry
+        sum = digit1 + digit2 + carry;
+
+        // Store the result digit in the result buffer
+        if (k < size_r - 1) // Reserve space for the null terminator
+        {
+            r[k] = sum % 10 + '0';
+            k++;
+        }
+        else
+        {
+            return 0; // Result cannot be stored in the buffer, return 0
+        }
+
+        // Update carry for the next iteration
+        carry = sum / 10;
+
+        // Move to the next digits of the input numbers
+        i--;
+        j--;
+    }
+
+    // Add null terminator to the result string
+    r[k] = '\0';
+
+    // Reverse the result string to get the correct order
+    for (i = 0, j = k - 1; i < j; i++, j--)
+    {
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
+    }
+
+    return r;
 }
